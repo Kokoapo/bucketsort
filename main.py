8b7ea39dea7c03043cc08cf64ad8984ebf144dbf
Kokoapo <viniviscdiniz@gmail.com>
2026-04-13 19:03:16 -0300

    main plus figures

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 from execute import execute
 
 sizes = [100000, 500000, 1000000]
-buckets = [1, 10, 100, 1000, 10000, 100000]
+buckets = [100, 1000, 10000, 100000]
 
 for size in sizes:
     times = []
@@ -10,12 +10,13 @@ for size in sizes:
         final_time = execute(size, bucket)
         times.append(final_time)
         print(f'Tamanho {size} Baldes {bucket} Tempo {final_time}')
+    plt.close()
     plt.plot(buckets, times)
 
     plt.xscale('log')
 
-    plt.xlabel('Tempo (Segundos)')
-    plt.ylabel('Número de Baldes')
+    plt.xlabel('Número de Baldes')
+    plt.ylabel('Tempo (Segundos)')
     plt.title(f'Execução para {size} Elementos')
 
     plt.savefig(f'./figures/{size}.png')
