6a44c118cea1ef23c00f94c29c5fafbeaa16e4d9
Kokoapo <viniviscdiniz@gmail.com>
2026-04-13 17:04:11 -0300

    main

---
+++ main.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+from execute import execute
+
+sizes = [100000, 500000, 1000000]
+buckets = [1, 10, 100, 1000, 10000, 100000]
+
+for size in sizes:
+    times = []
+    for bucket in buckets:
+        final_time = execute(size, bucket)
+        times.append(final_time)
+    plt.plot(times, buckets)
+    plt.xlabel('Tempo (Segundos)')
+    plt.ylabel('Número de Baldes')
+    plt.title(f'Execução para {size} Elementos')
+
+    plt.savefig(f'./figures/{size}.png')
