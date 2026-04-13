8e856f7edece178dac388ad2bf6c0c953c191946
Kokoapo <viniviscdiniz@gmail.com>
2026-04-13 15:29:49 -0300

    insertion sort

--- bucketsort.py
+++ bucketsort.py
@@ -1,3 +1,12 @@
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+
 def bucket_sort(arr, n_buckets):
     n = len(arr)
     buckets = [[] for _ in range(n_buckets)]
